8c740fb099bbf21ddaa0a8ad5ade13c9ef347afd
ekchoi391201 <ekchoi391201@gmail.com>
2025-06-09 16:16:15 +0900

    V1

--- app.py
+++ app.py
@@ -9,15 +9,15 @@ app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 # 세션을 파일 시스템에 저장
-app.config['SESSION_TYPE'] = 'filesystem'
-app.config['SESSION_FILE_DIR'] = './flask_session/'
-app.config['SESSION_PERMANENT'] = False
-app.config['SESSION_USE_SIGNER'] = True
-app.config['SESSION_KEY_PREFIX'] = 'session:'
-Session(app)
+#app.config['SESSION_TYPE'] = 'filesystem'
+#app.config['SESSION_FILE_DIR'] = './flask_session/'
+#app.config['SESSION_PERMANENT'] = False
+#app.config['SESSION_USE_SIGNER'] = True
+#app.config['SESSION_KEY_PREFIX'] = 'session:'
+#Session(app)
 
 # MySQL 설정
-app.config['MYSQL_HOST'] = 'DB주소'
+app.config['MYSQL_HOST'] = '10.0.64.100' #DB iP
 app.config['MYSQL_USER'] = 'frodo'
 app.config['MYSQL_PASSWORD'] = 'Frodo5020!!'
 app.config['MYSQL_DB'] = 'frodo'
